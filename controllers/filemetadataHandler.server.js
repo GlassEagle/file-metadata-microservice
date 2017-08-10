425ac30edcf082f08ae2c038a080fe1c6c99c798
glass eagle <mario.torres2@yahoo.com>
2017-08-10 05:52:35 +0000

    added return on no file uploaded

--- controllers/filemetadataHandler.server.js
+++ controllers/filemetadataHandler.server.js
@@ -4,6 +4,7 @@ function filemetadataHandler(req, res){
     if(!req.file){
         res.status(400);
         res.send("ERROR: no file uploaded");
+        return;
     }
     var data = {size: req.file.size};
     data = JSON.stringify(data);
