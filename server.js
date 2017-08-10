460da7ad414fbd502790e0c0068ec3d1428ce9b0
glass eagle <mario.torres2@yahoo.com>
2017-08-10 05:34:00 +0000

    remove mongoose

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 "use strict";
 
 var express = require("express");
-var mongoose = require("mongoose");
 var multer = require("multer");
 var filemetadataHandler = require("./controllers/filemetadataHandler.server.js");
 
 var upload = multer();
 var app = express();
-mongoose.Promise = global.Promise;
-
-mongoose.connect("mongodb://localhost:27017/data", {useMongoClient: true});
 
 app.get("/", function(req, res){
     res.sendFile( process.cwd() + "/public/index.html");
