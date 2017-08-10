ec0d59da3751fc712ee9472bcc130d67164a24f2
glass eagle <mario.torres2@yahoo.com>
2017-08-10 02:56:12 +0000

    mounted on /api/filemetadata

---
+++ controllers/filemetadataHandler.server.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function filemetadataHandler(req, res){
+    res.send("filemetadataHandler");
+}
+
+module.exports = filemetadataHandler;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 var express = require("express");
 var mongoose = require("mongoose");
+var filemetadataHandler = require("./controllers/filemetadataHandler.server.js");
 
 var app = express();
 mongoose.Promise = global.Promise;
@@ -11,6 +12,8 @@ mongoose.connect("mongodb://localhost:27017/data", {useMongoClient: true});
 app.get("/", function(req, res){
     res.sendFile( process.cwd() + "/public/index.html");
 });
+
+app.post("/api/filemetadata", filemetadataHandler);
 
 app.listen(process.env.PORT, function () {
   console.log('App listening on port %s!', process.env.PORT);
