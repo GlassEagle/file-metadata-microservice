cc7e39652fcbdf5ceba1a3969c708083b85b9af2
glass eagle <mario.torres2@yahoo.com>
2017-08-10 05:41:28 +0000

    handle no file uploaded

--- controllers/filemetadataHandler.server.js
+++ controllers/filemetadataHandler.server.js
@@ -1,6 +1,10 @@
 "use strict";
 
 function filemetadataHandler(req, res){
+    if(!req.file){
+        res.status(400);
+        res.send("ERROR: no file uploaded");
+    }
     var data = {size: req.file.size};
     data = JSON.stringify(data);
     res.setHeader("content-type", "application/json");
