286b1cfd64117ca829c9c6f97d8db90db75312a6
glass eagle <mario.torres2@yahoo.com>
2017-08-10 02:37:19 +0000

    mounted index.html on /

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ mongoose.Promise = global.Promise;
 
 mongoose.connect("mongodb://localhost:27017/data", {useMongoClient: true});
 
+app.get("/", function(req, res){
+    res.sendFile( process.cwd() + "/public/index.html");
+});
+
 app.listen(process.env.PORT, function () {
   console.log('App listening on port %s!', process.env.PORT);
 });
